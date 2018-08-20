18479d674071cd56c6493ca2aaf24d32c539ad70
Yanyiyi <ChunYan.liu@yannest.net>
2018-08-20 16:23:33 +0800

    update the Get URL function
    update the Get URL function

---
+++ js/liMain.js
@@ -0,0 +1,26 @@
+function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+};
+
+var lilis = [];
+var imglilitype = ['', '../img/icon_blue.png', '../img/icon_lightblue.png', '../img/icon_yellow.png', '../img/icon_red.png', '../img/icon_lime.png'];
+$.getJSON('https://spreadsheets.google.com/feeds/list/1eUgqe2z8gL1d9GrY2LwpAAxW9Wh2xOKOopqDNcISdpE/1/public/values?alt=json', function (dataLog) {
+
+        //        console.log("gJson");
+        var i = GetURLParameter("liliID");
+        //        console.log(i);
+        var aName = dataLog.feed.entry[i].gsx$liliname.$t;
+        //        var aLatitude = dataLog.feed.entry[i].gsx$lati.$t;
+        //        var aLongtitude = dataLog.feed.entry[i].gsx$longi.$t;
+        var alilitype = dataLog.feed.entry[i].gsx$lilitype.$t;
+        //        alert(aName);
+        $("#liName").text(aName);
+    } //end function data
+); //end get JSON
